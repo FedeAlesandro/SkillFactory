05b99eb92e5902649e2ef127eeacf571b961c935
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-07 00:39:40 -0300

    [Done] Collection changed, all methods done and removed the unnecesary

--- CollectionsInterface/Exercise2/src/Marathon.java
+++ CollectionsInterface/Exercise2/src/Marathon.java
@@ -1,19 +1,28 @@
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Marathon <T extends ISprintable>{
-    TreeSet <T> competitors;
+    private Double distance;
+    private List <T> competitors; //Uso List porque con TreeSet tenia que comparar y en este caso es innecesario, iba a terminar recorriendo todos los valores igual
 
-    public Marathon() {
-        this.competitors = new TreeSet<>();
+    public Marathon(Double distance) {
+        this.distance = distance;
+        this.competitors = new ArrayList<>();
     }
 
+    private Double getDistance(){
+        return this.distance;
+    }
     public void add(T competitor){
         competitors.add(competitor);
     }
-    public TreeSet getCompetitors(){
-        return competitors;
+    public void printCompetitorsTime(){
+        for(T competitor : competitors){
+            Double time = competitor.run(this.getDistance());
+            time = Math.round(time * 100.00)/100.00;
+            System.out.println("Competitor: " + competitor.toString() + ". Time realized: " + time);
+        }
     }
-
     @Override
     public String toString() {
         return "Competitors: " + competitors;
