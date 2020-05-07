e8e15a25409e1d521df038b950429b5db150b5f0
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-07 11:02:46 -0300

    Project using a local dependency

---
+++ Strings/src/main/java/net/avalith/App.java
@@ -0,0 +1,14 @@
+package net.avalith;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Stack<String>stack = new Stack<>();
+        stack.push("Hello");
+        stack.push("Yes");
+        stack.push("No");
+        stack.push("Bye");
+        System.out.println(stack);
+    }
+}
