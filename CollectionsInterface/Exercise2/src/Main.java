dbe27be92218ff0d5f8b839936ab18c5aa1be984
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-07 00:37:35 -0300

    [Done] Main already made and all tested

--- CollectionsInterface/Exercise2/src/Main.java
+++ CollectionsInterface/Exercise2/src/Main.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Marathon marathon = new Marathon();
+        Marathon marathon = new Marathon(Double.valueOf(42195));
         marathon.add(new Human("Federico"));
         marathon.add(new Dog("Lobito"));
         marathon.add(new Robot("Arturito"));
 
-        System.out.println(marathon.getCompetitors());
+        marathon.printCompetitorsTime();
 
     }
 }
