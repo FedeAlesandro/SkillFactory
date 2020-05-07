202b60afc3d4ea9bfcdaba89f9b867dc484e2d83
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-07 14:39:15 -0300

    [Working] Testing methods in main

--- Strings/src/main/java/net/avalith/App.java
+++ Strings/src/main/java/net/avalith/App.java
@@ -4,11 +4,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        Stack<String>stack = new Stack<>();
+       /* Stack<String>stack = new Stack<>();
         stack.push("Hello");
         stack.push("Yes");
         stack.push("No");
         stack.push("Bye");
-        System.out.println(stack);
+        System.out.println(stack); */
+        System.out.println(StringsMethods.isPalindrome("Arriba la birra"));
+        System.out.println(StringsMethods.romanNumberToInteger("XXI"));
+        System.out.println(StringsMethods.reorderWords("Aguante Boca Juniors papá"));
     }
 }
