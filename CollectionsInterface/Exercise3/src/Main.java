e715a69a4c9c557fa5d4c3496bdf7a7d7f6d4301
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-07 16:08:22 -0300

    [Done] Method created and tested in main

--- CollectionsInterface/Exercise3/src/Main.java
+++ CollectionsInterface/Exercise3/src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
-
+	
+	public static void removeEvenLenght(ArrayList<String> strings){
+        for(int i = 0; i<strings.size(); i++){
+            int length = strings.get(i).length();
+            if(length % 2 == 0)
+                strings.remove(strings.get(i));
+        }
+    }
+	
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String>strings = new ArrayList<>();
+        strings.add("Federico");
+        strings.add("Ernesto");
+        strings.add("Jazmin");
+        strings.add("Jorge");
+        strings.add("Javier");
+        System.out.println(strings);
+        removeEvenLenght(strings);
+        System.out.println(strings);
     }
 }
