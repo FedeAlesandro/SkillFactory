26f22d942a2b151efb72a98241db3016cc96f910
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-12 16:20:55 -0300

    [Working] Last exercise doesnt work

--- Strings/src/main/java/net/avalith/App.java
+++ Strings/src/main/java/net/avalith/App.java
@@ -5,11 +5,11 @@ public class App
     public static void main( String[] args )
     {
         StringsMethods stringsMethods = new StringsMethods();
-//        System.out.println(stringsMethods.isPalindrome("Arriba la birra"));
-//        System.out.println(stringsMethods.romanNumberToInteger("XXI"));
-//        System.out.println(stringsMethods.reorderWords("Aguante Boca Juniors papá"));
-//        System.out.println(stringsMethods.countDiferentCaracters("Soy Independiente, aunque no gano la libertadores\n" + "desde 1985, sigo siendo el máximo campeón con 7 copas"));
-//        System.out.println(stringsMethods.caesarCipher("adegñzxi"));
+        System.out.println(stringsMethods.isPalindrome("Arriba la birra"));
+        System.out.println(stringsMethods.romanNumberToInteger("XXI"));
+        System.out.println(stringsMethods.reorderWords("Aguante Boca Juniors papá"));
+        System.out.println(stringsMethods.countDiferentCaracters("Soy Independiente, aunque no gano la libertadores\n" + "desde 1985, sigo siendo el máximo campeón con 7 copas"));
+        System.out.println(stringsMethods.caesarCipher("adegñzxi"));
         System.out.println(stringsMethods.verifyString("Hola1om_"));
     }
 }
