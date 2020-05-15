4278a1df1e9030b17bf5d997154317d1367e8bda
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-15 16:34:35 -0300

    [New] JDBCTest

---
+++ JDBCTest/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.connection.MyConnection;
+import org.example.model.Product;
+import org.example.service.CRUDProduct;
+
+import java.math.BigDecimal;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+       
+    }
+}
