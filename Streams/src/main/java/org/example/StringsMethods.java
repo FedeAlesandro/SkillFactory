c15c24b13de050fc1c7add76c82146649b3e0b6c
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-15 20:53:19 -0300

    [Update] Method improved

--- Streams/src/main/java/org/example/StringsMethods.java
+++ Streams/src/main/java/org/example/StringsMethods.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,26 +16,20 @@ public class StringsMethods {
                 .filter(string -> string.length() % 2 != 0)
                 .collect(Collectors.toList());
     }
-    public String countDiferentCaracters(String string){
-
-        AtomicInteger letters = new AtomicInteger(0);
-        AtomicInteger digits = new AtomicInteger(0);
-        AtomicInteger spaces = new AtomicInteger(0);
-
-        string.chars()
-                .mapToObj(chr -> String.valueOf((char) chr))
-                .filter(chr -> chr.matches("[A-Za-zÀ-ÖØ-öø-ÿ]"))
-                .forEach(chr -> letters.getAndIncrement());
-        string.chars()
+    public String countDifferentCharacters(String string){
+        
+        return "Digits: " + string.chars()
                 .mapToObj(chr -> String.valueOf((char) chr))
                 .filter(chr -> chr.matches("[0-9]"))
-                .forEach(chr -> digits.getAndIncrement());
-        string.chars()
+                .count()
+                + ", letters: " + string.chars()
+                .mapToObj(chr -> String.valueOf((char) chr))
+                .filter(chr -> chr.matches("[A-Za-zÀ-ÖØ-öø-ÿ]"))
+                .count()
+                + ", spaces: " + string.chars()
                 .mapToObj(chr -> String.valueOf((char) chr))
                 .filter(chr -> chr.matches(("[' ']")))
-                .forEach(chr -> spaces.getAndIncrement());
-
-        return "Digits: "+ digits + ", letters: " + letters + ", spaces: " + spaces;
+                .count();
     }
     // I'm not considering the ñ because instead of receiving a 164, I'm receiving another ASCII code, also when I print 'ñ' in a System.out,
     // it doesn't print as it should be and the UTF is right
